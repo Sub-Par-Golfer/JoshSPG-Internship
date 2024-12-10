11a664dfc84091e0bcb237651d1d5e6b323178ba
Sub-Par-Golfer <joshjthompson@yahoo.com>
2024-12-09 22:48:07 -0500

    Implement changed from useFetch to Axios and added skeleton loading state, al so fixed the padding between each tile

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,25 +1,111 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
-import useFetch from "../useFetch";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import axios from "axios";
 
 const HotCollections = () => {
-  const { data, loading, error } = useFetch(
-    "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
-  );
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+  useEffect(() => {
+    const fetchCollections = async () => {
+      try {
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+        );
+        // Simulate delay
+        setTimeout(() => {
+          setData(response.data);
+          setLoading(false);
+        }, 2000);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
 
-  // Slider settings
+    fetchCollections();
+  }, []);
+
   const settings = {
-    infinite: true, // Loop slides
-    speed: 500, // Transition speed
-    slidesToShow: 4, // Number of slides visible
-    slidesToScroll: 1, // Number of slides to scroll on arrow click
-    autoplay: true, // Auto-slide
-    autoplaySpeed: 3000, // Auto-slide interval
-    arrows: true, // Show arrows for navigation
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 870,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
+
+  if (loading) {
+    return (
+      <section id="section-collections" className="no-bottom">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="text-center">
+                <h2>Hot Collections</h2>
+                <div className="small-border bg-color-2"></div>
+              </div>
+            </div>
+            <div className="col-lg-12">
+              <Slider {...settings}>
+                {Array.from({ length: 4 }).map((_, index) => (
+                  <div className="nft_coll_wrapper" key={index}>
+                    <div className="nft_coll">
+                      <div className="nft_wrap">
+                        <Skeleton height={150} width="100%" />
+                      </div>
+                      <div className="nft_coll_pp">
+                        <Skeleton circle height={50} width={50} />
+                      </div>
+                      <div className="nft_coll_info">
+                        <h4>
+                          <Skeleton width="60%" />
+                        </h4>
+                        <span>
+                          <Skeleton width="40%" />
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
   return (
     <section id="section-collections" className="no-bottom">
@@ -34,25 +120,27 @@ const HotCollections = () => {
           <div className="col-lg-12">
             <Slider {...settings}>
               {data.map((collection) => (
-                <div className="nft_coll" key={collection.id}>
-                  <div className="nft_wrap">
-                    <img
-                      src={collection.nftImage}
-                      className="lazy img-fluid"
-                      alt={collection.name}
-                    />
-                  </div>
-                  <div className="nft_coll_pp">
-                    <img
-                      className="lazy pp-coll"
-                      src={collection.authorImage}
-                      alt={collection.author}
-                    />
-                    <i className="fa fa-check"></i>
-                  </div>
-                  <div className="nft_coll_info">
-                    <h4>{collection.title}</h4>
-                    <span>ERC - {collection.code}</span>
+                <div className="nft_coll_wrapper" key={collection.id}>
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <img
+                        src={collection.nftImage}
+                        className="lazy img-fluid"
+                        alt={collection.name}
+                      />
+                    </div>
+                    <div className="nft_coll_pp">
+                      <img
+                        className="lazy pp-coll"
+                        src={collection.authorImage}
+                        alt={collection.author}
+                      />
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <h4>{collection.title}</h4>
+                      <span>{collection.code}</span>
+                    </div>
                   </div>
                 </div>
               ))}
